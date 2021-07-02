cc8a8d5fe91d4f34e102c3bbab2a447daf971d69
Markko Merzin <markko.merzin@gmail.com>
2021-07-03 00:56:39 +0300

    tcp support for navionics tablet

--- nmea0183_repeater.py
+++ nmea0183_repeater.py
@@ -9,6 +9,8 @@ import threading
 import sys
 import queue
 import signal
+import select
+import socket
 
 MAX_WRITE_QUEUE_LEN = 100 # how many messages to hold in write queue before starting to drop messages
 
@@ -47,6 +49,12 @@ def main():
         else:
             print("{}: WARN: No config found for {}".format(sys.argv[0], dev_hook), file=sys.stderr)
 
+    for config in configurations["configurations"]:
+        if "network_type" in config and config["network_type"] == "tcp_server":
+            new_tcp_server_thread = nmea0183_tcp_server(config, repeat_message)
+            new_tcp_server_thread.start()
+            writer_threads.append(new_tcp_server_thread)
+
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGHUP, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
@@ -73,7 +81,7 @@ def repeat_message(name, message):
 
 def match_configuration(udev_path, configurations):
     for config in configurations["configurations"]:
-        if udev_path.startswith(config["port_device_prefix"]):
+        if "port_device_prefix" in config and udev_path.startswith(config["port_device_prefix"]):
             return config
     else:
         return None
@@ -106,7 +114,7 @@ class nmea0183_reader(threading.Thread):
     def run(self):        
         charbuf = bytearray()
         while continue_work:
-            char = self.ser.read(size=1)
+            char = self.ser.read(size=1) # blocks until timeout is reached. Timeout is defined when creating serial.Serial object.
             if len(char) > 0:
                 charbuf.extend(char)
                 if char == b'\n':
@@ -158,6 +166,116 @@ class nmea0183_writer(threading.Thread):
             if debug_flag:
                 print("{}: ERR: Dropping messages to {}".format(sys.argv[0], self.name), file=sys.stderr)
 
+class nmea0183_tcp_server(threading.Thread):
+    def __init__(self, config, callback):
+        threading.Thread.__init__(self)
+        
+        self.config = config
+        self.callback = callback
+        self.name = config["name"]
+        self.write_queue = queue.Queue()
+        tcpport = int(config["network_port"])
+        
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.setblocking(0)        
+        self.server.bind(('0.0.0.0', tcpport))
+        self.server.listen(5)
+        self.inputs = [self.server]
+        self.outputs = []
+        self.message_queues = {}
+        self.line_buffers = {}
 
+        if "accept_messages" in config:
+            self.accept_messages = set(config["accept_messages"])
+        else:
+            self.accept_messages = None
+
+        if "deny_messages" in config:
+            self.deny_messages = set(config["deny_messages"])
+        else:
+            self.deny_messages = None
+
+    def run(self):
+        while self.inputs and continue_work:
+            readable, writable, exceptional = select.select(self.inputs, self.outputs, self.inputs, 1)
+            for s in readable:
+                if s is self.server:
+                    connection, client_address = s.accept()
+                    connection.setblocking(0)
+                    self.inputs.append(connection)
+                    self.message_queues[connection] = queue.Queue()
+                else:                    
+                    data = s.recv(1024)
+                    if data:
+                        if s not in self.line_buffers:
+                            self.line_buffers[s] = bytearray()
+                        
+                        for char in [bytes([b]) for b in data]:
+                            self.line_buffers[s].extend(char)
+                            if char == b'\n':                                
+                                line = self.line_buffers[s].decode("iso-8859-1")
+                                self.line_buffers[s].clear()
+
+                                # send to serials
+                                self.callback(self.config["name"], line)
+
+                                # send to other tcp clients connected to this port
+                                # we need to process it here as regular send() framework
+                                # does not know about sockets and we would get a local echo
+                                repeat_message = True
+                                message_type = line[3:6]
+                                if self.accept_messages is not None:
+                                    if message_type not in self.accept_messages:
+                                        repeat_message = False
+                                if self.deny_messages is not None:
+                                    if message_type in self.deny_messages:
+                                        repeat_message = False
+                                if repeat_message:                                                                
+                                    for mq_key in self.message_queues:
+                                        if mq_key != s:
+                                            self.message_queues[mq_key].put(bytes(line, "iso-8859-1"))
+                                            if mq_key not in self.outputs:
+                                                self.outputs.append(mq_key)                                                        
+                    else:
+                        if s in self.outputs:
+                            self.outputs.remove(s)
+                        self.inputs.remove(s)
+                        if s in self.line_buffers:
+                            del self.line_buffers[s]
+                        s.close()
+                        del self.message_queues[s]                                                
+
+            for s in writable:
+                try:
+                    next_msg = self.message_queues[s].get_nowait()
+                except queue.Empty:
+                    self.outputs.remove(s)
+                else:
+                    s.send(next_msg)
+
+            for s in exceptional:
+                self.inputs.remove(s)
+                if s in self.outputs:
+                    self.outputs.remove(s)
+                if s in self.line_buffers:
+                    del self.line_buffers[s]
+                s.close()
+                del self.message_queues[s]
+
+        def send(self, message):
+            message_type = message[3:6]
+            if self.accept_messages is not None:
+                if message_type not in self.accept_messages:
+                    return
+            if self.deny_messages is not None:
+                if message_type in self.deny_messages:
+                    return
+
+            for mq_key in self.message_queues:                
+                self.message_queues[mq_key].put(message)
+                if mq_key not in self.outputs:
+                    self.outputs.append(mq_key)
+            
 if __name__ == "__main__":
     main()
